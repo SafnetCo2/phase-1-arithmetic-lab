d98f799269ced20b8819237a51398b38a8d99b94
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-28 13:18:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 4;
+let num4 = 16;
+const mod = num3 % num4;
+const max = Math.max(2, 5, 10, 15, 20);
+
+
